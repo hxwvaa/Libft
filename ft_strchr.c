4df8278484b3afce80dd5769b31b2422571ce402
hamdhan <hamdhan@ubuntu.myguest.virtualbox.org>
2024-03-25 15:59:06 +0400

    strrchr done

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,17 +1,17 @@
 #include "libft.h"
 
-char	*ft_strchr(const char *str, int c)
+char	*ft_strchr(const char *s, int c)
 {
 	int	i;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (s[i] != '\0')
 	{
-		if (str[i] == c)
-			return ((char *)str + i);
+		if (s[i] == c)
+			return ((char *)s + i);
 		i++;
 	}
 	if (c == '\0')
-		return ((char *)str + i);
+		return ((char *)s + i);
 	return (NULL);
 }
