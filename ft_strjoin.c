8588e7684da13242812e2f66f5f98b83c4cd37aa
hxwvaa <hamdhann11@gmail.com>
2024-04-29 12:21:03 +0400

    ft_substr.c done, check ft_strjoin.c again

---
+++ ft_strjoin.c
@@ -0,0 +1,30 @@
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	unsigned int	i;
+	unsigned int	j;
+	char			*str;
+
+	i = ft_strlen(s1) - 1;
+	j = ft_strlen(s2);
+	str = malloc(i + j);
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = i;
+	i = 0;
+	while (s2[i] != '\0')
+	{
+		str[j] = s2[i];
+		i++;
+		j++;
+	}
+	str[j] = '\0';
+	return (str);
+}
---
+++ ft_substr.c
@@ -0,0 +1,20 @@
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	unsigned int	i;
+	char			*str;
+
+	i = 0;
+	str = malloc(len + 1);
+	if (str == NULL)
+		return (NULL);
+	while (i < len && s[start] != '\0')
+	{
+		str[i] = s[start];
+		start++;
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
