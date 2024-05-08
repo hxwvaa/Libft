2fbc8cd7862dae71b342e3adef854014f75228c0
hxwvaa <hamdhann11@gmail.com>
2024-05-08 15:12:37 +0400

    libft.h

--- libft.h
+++ libft.h
@@ -7,5 +7,6 @@ size_t	ft_strlen(const char *s);
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_calloc(size_t nmemb, size_t size);
 void	ft_bzero(void *s, size_t n);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 
 #endif
