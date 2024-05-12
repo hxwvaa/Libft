4750c5b87f9bb0a44e817ed197fc2b2da6e326df
hxwvaa <hamdhann11@gmail.com>
2024-05-12 17:21:59 +0400

    madatory complete and tested

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,9 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	size_t	j;
 
 	i = 0;
-	if (little == NULL || little[0] == '\0')
+	if (little[0] == '\0')
+		return ((char *)big);
+	if (!little)
 		return ((char *)big);
 	while (big[i] && i < len)
 	{
